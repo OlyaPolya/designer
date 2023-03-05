3cd4a2feacb2227e3ef327023abdde5cb08d9166
Olya <polyanskaya3000@gmail.com>
2023-03-06 02:37:07 +0700

    init: basic project setup

---
+++ src/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+import MyApp from './Header/Header';
+
+function App() {
+  return (
+    <main className="App">
+      <MyApp />
+    </main>
+  );
+}
+
+export default App;
