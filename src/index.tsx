af28df8722acf3e1cdc389311cb46b4509048997
Olga Polyanskaya <polyanskaya3000@gmail.com>
2023-11-07 01:57:02 +0700

    ref: change file name

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from './App/App';
+import App from './App';
 import ErrorPage from './App/pages/Error';
 import Modal, { loader as appLoader } from './App/pages/Main/Modal';
 
